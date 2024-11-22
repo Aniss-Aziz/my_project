3c0cd8f48ddb73c4398a6ea93da8a6b99a07b20e
Aniss Aziz <anissaziz31000@gmail.com>
2024-11-22 11:23:15 +0100

    separate the project

--- backend/routes/userRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const authController = require('../controllers/authController');
-const loginController = require('../controllers/loginController');
-
-router.post('/register', authController.register);
-router.post('/login', loginController.login);
-
-module.exports = router;
